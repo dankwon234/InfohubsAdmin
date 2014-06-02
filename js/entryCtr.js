c4354730ca8afbb06f90e431a5e7df597b6af9ec
Parker Stakoff <pssoftware17@gmail.com>
2014-06-02 13:45:43 -0400

    mime?

--- js/entryCtr.js
+++ js/entryCtr.js
@@ -11,7 +11,7 @@ app.controller("EntryController", function($scope, $http, $upload){
 	
     $scope.init = function() {
     	console.log('HOME CTR INIT');
-    	fetchEntries();
+//    	fetchEntries();
 //    	fetchDevices();
     }    
     
