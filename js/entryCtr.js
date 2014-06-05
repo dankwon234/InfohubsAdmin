6fb4dd6cf441eaae3277b645ff4755b9c06fd8f8
Parker Stakoff <pssoftware17@gmail.com>
2014-06-05 13:20:32 -0400

    drop logo test

--- js/entryCtr.js
+++ js/entryCtr.js
@@ -1,4 +1,4 @@
-var app = angular.module('Entry', []);
+var app = angular.module('Entry', ['angularFileUpload']);
 
 app.controller("EntryController", function($scope, $http){
 	$scope.loading = false;
@@ -182,7 +182,81 @@ app.controller("EntryController", function($scope, $http){
 		    console.log("error", data, status, headers, config);
 		});
 	}
-    
+	
+	$scope.onFileSelect = function($files, property) {
+	    $scope.loading = true;
+    	console.log('SELECT IMAGE: '+property);
+//        var url = '/api/upload?resource=entry&property='+property+'&id='+$scope.selectedDevice.uuid;
+        var url = '/api/upload?resource=entry';
+    	
+    	
+        $http.get(url).success(function(data, status, headers, config) {
+            results = data['results'];
+            confirmation = results['confirmation'];
+            if (confirmation=='success'){
+            	uploadString = results['upload'];
+                console.log('UPLOAD STRING: '+uploadString);
+                
+            	
+                //$files: an array of files selected, each file has name, size, and type.
+                for (var i = 0; i < $files.length; i++) {
+                  var file = $files[i];
+                  $scope.upload = $upload.upload({
+                    url: uploadString, //upload.php script, node.js route, or servlet url
+                    method: 'POST',
+                    // headers: {'header-key': 'header-value'},
+                    // withCredentials: true,
+                    data: {myObj: $scope.myModelObj},
+                    file: file // or list of files: $files for html5 only
+                    /* set the file formData name ('Content-Desposition'). Default is 'file' */
+                    //fileFormDataName: myFile, //or a list of names for multiple files (html5).
+                    /* customize how data is added to formData. See #40#issuecomment-28612000 for sample code */
+                    //formDataAppender: function(formData, key, val){}
+                  }).progress(function(evt) {
+                    console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+                  }).success(function(data, status, headers, config) {
+                    // file is uploaded successfully
+                    console.log(data);
+                    
+                    results = data['results'];
+                    
+                    //TODO: check confirmation key for success
+                    confirmation = results['confirmation'];
+            	    $scope.loading = false;
+                    if (confirmation=='success'){
+                    	
+                    	console.log(JSON.stringify(results));
+/*
+                        updatedDevice = results['device'];
+                        
+                        if (property=='logo'){
+                        	$scope.selectedDevice.image = updatedDevice['image'];
+                        }
+*/
+                    }
+                    else{
+                    	alert(results['message']);
+                    }
+                  });
+                  //.error(...)
+                  //.then(success, error, progress); 
+                  //.xhr(function(xhr){xhr.upload.addEventListener(...)})// access and attach any event listener to XMLHttpRequest.
+                }
+                
+                /* alternative way of uploading, send the file binary with the file's content-type.
+                   Could be used to upload files to CouchDB, imgur, etc... html5 FileReader is needed. 
+                   It could also be used to monitor the progress of a normal http post/put request with large data*/
+                // $scope.upload = $upload.http({...})  see 88#issuecomment-31366487 for sample code.
+            }
+            else {
+        	    $scope.loading = false;
+                alert(results['message']);
+            }
+        }).error(function(data, status, headers, config) {
+    	    $scope.loading = false;
+            console.log("error", data, status, headers, config);
+        });
+      };
     
 });
 
