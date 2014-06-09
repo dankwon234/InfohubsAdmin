abb7eab592bd11ad92aaaa6f5fa288dc422f3cd4
Parker Stakoff <pssoftware17@gmail.com>
2014-06-09 13:18:06 -0400

    s

--- js/entryCtr.js
+++ js/entryCtr.js
@@ -91,9 +91,7 @@ if ($scope.searchEntry.length<1){
     
     $scope.selectFoursquareEntry = function(index){
     	$scope.selectedEntry = $scope.searchResults.foursquare[index];
-    	console.log('Search Foursquare Entry: '+JSON.stringify(entry));
-    	
-		$scope.primaryURL = "test";
+    	console.log('Search Foursquare Entry: '+JSON.stringify($scope.selectedEntry));
     	
     }
     
